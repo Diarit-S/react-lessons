ef2a54d125b2c4d1ecbcff5a2e612d26b0071901
Diarit-S <diarit-94@hotmail.fr>
2019-06-25 20:36:29 +0200

    lesson compnents

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+class Composant extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      compteur: 0,
+    }
+  }
+
+  increment = ()=>{
+    this.setState({
+      compteur : this.state.compteur + 1
+    })
+  }
+
+  render(){
+    return (
+      <div>
+        <span>{this.state.compteur}</span>
+        <br/>
+        <button onClick={ this.increment }>+1</button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Composant name="World"/>, document.getElementById('root'));
